115cfab281a839c3a3def18631facfad7272d7b6
aacadavid <aacadavid@unicesar.edu.co>
2026-04-19 22:17:55 -0500

    feat: creacion de metodo para mostrar carta aplicando constantes de formato

--- src/main/java/com/mycompany/restauranteelbuensabor/Imprimir.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/Imprimir.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.restauranteelbuensabor;
 
+import java.util.List;
+
 /**
  *
  * @author alfre
@@ -16,6 +18,17 @@ public class Imprimir {
     private static final String separador = "========================================";
     private static final String lineaSimple = "----------------------------------------";
 
+    public static void mostrarCarta(List<Producto> menu){
 
+        System.out.println(separador);
+        System.out.println("   " + Datos.nombreRestaurante.toUpperCase());
+        System.out.println("   --- NUESTRA CARTA ---");
+        System.out.println(separador);
+
+        for (int i = 0; menu.size() < 10; i++) {
+            Producto p = menu.get(i);
+            System.out.printf("%d. %-22s $%,.0f%n", (i + 1), p.getNombre(), p.getPrecio());
+        }
+    }
     
 }
